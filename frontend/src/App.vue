8ba57bf3d3ae56c29ee77b54fbae55c4ab09e18f
rbrauner <rafalbrauner@gmail.com>
2021-10-09 16:13:36 +0200

    Add bootstrap, popperjs, fontawesome

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,23 @@
     <footer-component/>
 </template>
 
+<style lang="scss">
+// libs
+@import "bootstrap/scss/bootstrap";
+
+$fa-font-path: "~@fortawesome/fontawesome-free/webfonts";
+@import "~@fortawesome/fontawesome-free/scss/fontawesome";
+@import "~@fortawesome/fontawesome-free/scss/solid";
+@import "~@fortawesome/fontawesome-free/scss/brands";
+@import "~@fortawesome/fontawesome-free/scss/regular";
+</style>
+
 <script>
+// libs
+import "bootstrap";
+import "bootstrap";
+
+// components
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import NavComponent from "@/components/NavComponent.vue";
 import MainComponent from "@/components/MainComponent.vue";
