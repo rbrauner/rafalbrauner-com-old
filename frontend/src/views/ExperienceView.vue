f167410f1146b9d454eccf5539f9ffde7f733ea9
rbrauner <rafalbrauner@gmail.com>
2021-10-09 16:38:57 +0200

    ExperienceView content

--- frontend/src/views/ExperienceView.vue
+++ frontend/src/views/ExperienceView.vue
@@ -1,86 +1,33 @@
 <template>
     <section class="resume-section" id="experience">
         <div class="resume-section-content">
-            <h2 class="mb-5">Experience</h2>
+            <h2 class="mb-5">Umiejętności</h2>
             <div
                 class="d-flex flex-column flex-md-row justify-content-between mb-5"
             >
                 <div class="flex-grow-1">
-                    <h3 class="mb-0">Senior Web Developer</h3>
+                    <h3 class="mb-0">Backend Developer</h3>
                     <div class="subheading mb-3">
-                        Intelitec Solutions
+                        NoMonday
                     </div>
-                    <p>
-                        Bring to the table win-win survival strategies to ensure
-                        proactive domination. At the end of the day, going
-                        forward, a new normal that has evolved from generation X
-                        is on the runway heading towards a streamlined cloud
-                        solution. User generated content in real-time will have
-                        multiple touchpoints for offshoring.
-                    </p>
                 </div>
                 <div class="flex-shrink-0">
-                    <span class="text-primary">March 2013 - Present</span>
+                    <span class="text-primary">Październik 2019 - Teraz</span>
                 </div>
             </div>
             <div
                 class="d-flex flex-column flex-md-row justify-content-between mb-5"
             >
                 <div class="flex-grow-1">
-                    <h3 class="mb-0">Web Developer</h3>
+                    <h3 class="mb-0">Junior Backend Developer</h3>
                     <div class="subheading mb-3">
-                        Intelitec Solutions
+                        NoMonday
                     </div>
-                    <p>
-                        Capitalize on low hanging fruit to identify a ballpark
-                        value added activity to beta test. Override the digital
-                        divide with additional clickthroughs from DevOps.
-                        Nanotechnology immersion along the information highway
-                        will close the loop on focusing solely on the bottom
-                        line.
-                    </p>
                 </div>
                 <div class="flex-shrink-0">
-                    <span class="text-primary">December 2011 - March 2013</span>
-                </div>
-            </div>
-            <div
-                class="d-flex flex-column flex-md-row justify-content-between mb-5"
-            >
-                <div class="flex-grow-1">
-                    <h3 class="mb-0">Junior Web Designer</h3>
-                    <div class="subheading mb-3">
-                        Shout! Media Productions
-                    </div>
-                    <p>
-                        Podcasting operational change management inside of
-                        workflows to establish a framework. Taking seamless key
-                        performance indicators offline to maximise the long
-                        tail. Keeping your eye on the ball while performing a
-                        deep dive on the start-up mentality to derive
-                        convergence on cross-platform integration.
-                    </p>
-                </div>
-                <div class="flex-shrink-0">
-                    <span class="text-primary">July 2010 - December 2011</span>
-                </div>
-            </div>
-            <div class="d-flex flex-column flex-md-row justify-content-between">
-                <div class="flex-grow-1">
-                    <h3 class="mb-0">Web Design Intern</h3>
-                    <div class="subheading mb-3">
-                        Shout! Media Productions
-                    </div>
-                    <p>
-                        Collaboratively administrate empowered markets via
-                        plug-and-play networks. Dynamically procrastinate B2C
-                        users after installed base benefits. Dramatically
-                        visualize customer directed convergence without
-                        revolutionary ROI.
-                    </p>
-                </div>
-                <div class="flex-shrink-0">
-                    <span class="text-primary">September 2008 - June 2010</span>
+                    <span class="text-primary"
+                    >Cerwiec 2018 - Październik 2019</span
+                    >
                 </div>
             </div>
         </div>
