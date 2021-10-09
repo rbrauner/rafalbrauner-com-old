7eeaea8082aaaa50fac99d60ec22a9c9d6641595
rbrauner <rafalbrauner@gmail.com>
2021-10-09 16:47:23 +0200

    Add transition

--- frontend/src/components/MainComponent.vue
+++ frontend/src/components/MainComponent.vue
@@ -1,7 +1,11 @@
 <template>
     <main>
         <div class="container-fluid p-0">
-            <router-view/>
+            <router-view v-slot="{ Component }">
+                <transition name="fade" mode="out-in">
+                    <component :is="Component"/>
+                </transition>
+            </router-view>
         </div>
     </main>
 </template>
@@ -11,3 +15,15 @@ export default {
     name: "MainComponent"
 };
 </script>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
