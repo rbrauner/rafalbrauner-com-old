54c5b278db5d1fce44a2502b22eb16032ad10416
rbrauner <rafalbrauner@gmail.com>
2021-10-09 16:07:18 +0200

    Clear workspace

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,15 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import IndexView from "../views/IndexView.vue";
-
-const routes: Array<RouteRecordRaw> = [
+const routes = [
     {
         path: "/",
         name: "Index",
         component: IndexView,
     },
 ];
-
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
     routes,
 });
-
 export default router;
+//# sourceMappingURL=index.js.map
