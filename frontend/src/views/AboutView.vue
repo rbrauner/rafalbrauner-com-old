ae8b4b56822a14770eda67902b568de6f63dde6d
rbrauner <rafalbrauner@gmail.com>
2021-10-09 16:38:10 +0200

    AboutView content

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -2,31 +2,45 @@
     <section class="resume-section" id="about">
         <div class="resume-section-content">
             <h1 class="mb-0">
-                Clarence
-                <span class="text-primary">Taylor</span>
+                Rafał
+                <span class="text-primary">Brauner</span>
             </h1>
             <div class="subheading mb-5">
-                3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
-                <a href="mailto:name@email.com">name@email.com</a>
+                Rybnik, Polska
+                <a href="mailto:rafalbrauner@gmail.com"
+                >rafalbrauner@gmail.com</a
+                >
             </div>
             <p class="lead mb-5">
-                I am experienced in leveraging agile frameworks to provide a
-                robust synopsis for high level overviews. Iterative approaches
-                to corporate strategy foster collaborative thinking to further
-                the overall value proposition.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                accusamus consequuntur, nihil inventore facilis fugiat unde in,
+                repellat vero, rerum voluptates delectus possimus dolorum eius!
+                Necessitatibus, recusandae optio? Voluptatem, eaque!
             </p>
             <div class="social-icons">
-                <a class="social-icon" href="#"
-                    ><i class="fab fa-linkedin-in"></i
+                <a
+                    class="social-icon"
+                    href="https://www.linkedin.com/in/rbrauner"
+                    target="_blank"
+                ><i class="fab fa-linkedin-in"></i
                 ></a>
-                <a class="social-icon" href="#"
-                    ><i class="fab fa-github"></i
+                <a
+                    class="social-icon"
+                    href="https://gitlab.com/rbrauner"
+                    target="_blank"
+                ><i class="fab fa-gitlab"></i
                 ></a>
-                <a class="social-icon" href="#"
-                    ><i class="fab fa-twitter"></i
+                <a
+                    class="social-icon"
+                    href="https://github.com/rbrauner"
+                    target="_blank"
+                ><i class="fab fa-github"></i
                 ></a>
-                <a class="social-icon" href="#"
-                    ><i class="fab fa-facebook-f"></i
+                <a
+                    class="social-icon"
+                    href="https://www.facebook.com/rbrauner98"
+                    target="_blank"
+                ><i class="fab fa-facebook-f"></i
                 ></a>
             </div>
         </div>
